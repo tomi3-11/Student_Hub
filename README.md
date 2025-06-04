8cd546542374d11f95d6e58a235ed7d3ed6f01bc
thomas <didymusthomas6@gmail.com>
2025-06-04 22:25:13 +0300

    First Version 1.0

---
+++ README.md
@@ -0,0 +1,17 @@
+# Student Hub
+##
+##A platform where students track their academic life with ease. Build by student who faced a problem and are solving it with Student Hub
+
+## How to contribute
+
+### - 1. Clone the repository
+``` git clone https://github.com/tomi3-11/Student_Hub.git ```
+
+### - 2. Enter the project folder
+``` cd Student_Hub ```
+
+### - 3. Start contributing
+
+## Enjoy Creating Solutions to Real-World Problems.
+
+happy coding... 
---
+++ static/JS/student_hub.js
@@ -0,0 +1,17 @@
+const links = document.querySelectorAll('.nav-link');
+const pages = document.querySelectorAll('.page');
+
+links.forEach(link => {
+    link.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        // Remove active class from all links and pages
+        links.forEach(l => l.classList.remove('active'));
+        pages.forEach(p => p.classList.remove('active'));
+
+        // Add active class to clicked link and matching page
+        this.classList.add('active');
+        const target = document.getElementById(this.getAttribute("data-target"));
+        target.classList.add('active');
+    })
+})
